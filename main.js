39fcdfc33f7a193290cc91c21d0f3730cee49122
Anagha S R <88838571+MoLuTtY@users.noreply.github.com>
2021-08-23 11:54:17 +0530

    Added files

---
+++ main.js
@@ -0,0 +1,8 @@
+function handleclick() {
+    var x= document.getElementById("hi");
+    if(x.style.color=="white")
+        x.style.color="black";
+    else
+        x.style.color="white";
+    
+}
